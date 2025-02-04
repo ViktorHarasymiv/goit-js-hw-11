0ca886d6f21f8ba3a25fedbc787342e9c1b44083
Viktor <victorharasymiv97@gmail.com>
2025-02-04 02:49:14 +0100

    fixed

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,93 +1,59 @@
-import { createMarkup } from "./render-functions";
-
 // Описаний у документації
 import iziToast from "izitoast";
 // Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
 
+
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-const listImages = document.querySelector('.gallery');
-const loader = document.querySelector('.loader');
 
-const seurch_button = document.querySelector('.get--seurch_button');
-let formValue = document.querySelector('#form--input');
+import { createMarkup } from "./render-functions";
 
-const apiKey = "48554296-d2b735e3ffae040ef4553c6bb";
-let inputValue = "";
 
-formValue.addEventListener('input' , function(event) {
-    event.preventDefault();
+/*   Variables   */ 
 
-    inputValue = event.target.value.trim();
+export const listImages = document.querySelector('.gallery');
+export const loader = document.querySelector('.loader');
+
+const refreshPage = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
 });
 
-loader.style.display = 'none';
+/*  Function  */
 
-seurch_button.addEventListener("click" , (event) => { 
-    event.preventDefault();
-
-    listImages.innerHTML = "";
-
-    let url = `https://pixabay.com/api/?key=${apiKey}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true`;
-
-    if(inputValue.length === 0 || formValue.value === "") {
-
-        iziToast.warning({
-            title: 'Warning',
-            message: 'Please, enter a value!',
-
-            titleColor: 'white',
-            messageColor: 'white',
-            titleSize: '12px',
-            backgroundColor: '#ef4040',
-
-            iconColor: 'white',
-            position: 'topRight'
-        });
-
-        return false;
-    }
-
-    fetch(url)
+export function getImages(api) {
+    fetch(api)
     .then((response) => response.json())
     .then((data) => {
 
         if (!data.hits.length) {
             iziToast.warning({
-                title: 'Warning',
-                message: 'Sorry, there are no images matching your search query. Please try again!',
+               title: 'Warning',
+               message: 'Sorry, there are no images matching your search query. Please try again!',
 
-                titleColor: 'white',
-            messageColor: 'white',
-            titleSize: '12px',
-            backgroundColor: '#ef4040',
+               titleColor: 'white',
+               messageColor: 'white',
+               titleSize: '12px',
+               backgroundColor: '#ef4040',
 
-            iconColor: 'white',
-            position: 'topRight'
+               iconColor: 'white',
+               position: 'topRight'
             });
         }
 
         loader.style.display = 'block';
 
-            setTimeout(() => {
-              listImages.innerHTML = ("beforeend", createMarkup(data.hits));
+        listImages.innerHTML = ("beforeend", createMarkup(data.hits));
 
-              loader.style.display = 'none';
+        loader.style.display = 'none';
 
-              const refreshPage = new SimpleLightbox('.gallery a', {
-                captions: true,
-                captionsData: 'alt',
-                captionDelay: 250,
-              });
-              refreshPage.refresh();   
-            }, 1500);
-
-            formValue.value = "";
+        refreshPage.refresh();   
+        
  })
     .catch((error) => {
-        console.log(error.message);
         iziToast.error({
             title: 'Error',
             message: 'Something went wrong. Please try again later.',
@@ -101,4 +67,4 @@ seurch_button.addEventListener("click" , (event) => {
             position: 'topRight'
         });
     });
-});
+}
--- src/main.js
+++ src/main.js
@@ -1,2 +1,55 @@
-import "./js/pixabay-api.js";
-import "./js/render-functions.js";
+import { listImages } from "./js/pixabay-api";
+import { getImages } from "./js/pixabay-api";
+import { loader } from "./js/pixabay-api";
+
+
+/*   A P I  S E R V I E S   */ 
+
+/*   ↓   */
+
+
+/*   Variables   */ 
+
+const form = document.querySelector(".form");
+
+const apiKey = "48554296-d2b735e3ffae040ef4553c6bb";
+
+
+/*  Styles  */
+
+loader.style.display = 'none';
+
+
+/*  Event Listener  */
+
+form.addEventListener("submit" , (event) => {
+     
+    event.preventDefault();
+
+    let inputValue = form.seurch.value.trim();
+    let url = `https://pixabay.com/api/?key=${apiKey}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+    listImages.innerHTML = "";
+
+
+    if(inputValue.length === 0 || inputValue.value === "") {
+
+        iziToast.warning({
+            title: 'Warning',
+            message: 'Please, enter a value!',
+
+            titleColor: 'white',
+            messageColor: 'white',
+            titleSize: '12px',
+            backgroundColor: '#ef4040',
+
+            iconColor: 'white',
+            position: 'topRight'
+        });
+
+        return;
+    }
+
+    getImages(url );
+    form.reset();
+});
