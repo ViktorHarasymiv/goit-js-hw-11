1a3b215b5820d076131e0a2657e8c40cf987349e
ViktorHarasymiv <115593029+ViktorHarasymiv@users.noreply.github.com>
2025-02-07 23:35:09 +0000

    Deploying to gh-pages from @ ViktorHarasymiv/goit-js-hw-11@232e6531a52eeea294969a6afadf762a1ddc4dd6 🚀

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import{S as m,a as d,i as n}from"./assets/vendor-CUAzmrUA.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const i of e)if(i.type==="childList")for(const s of i.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&a(s)}).observe(document,{childList:!0,subtree:!0});function r(e){const i={};return e.integrity&&(i.integrity=e.integrity),e.referrerPolicy&&(i.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?i.credentials="include":e.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function a(e){if(e.ep)return;e.ep=!0;const i=r(e);fetch(e.href,i)}})();function p(o){return o.map(({webformatURL:t,largeImageURL:r,tags:a,likes:e,views:i,comments:s,downloads:h})=>`<li class="gallery--list">
-            <a class="gallery--link" href="${r}">
+import{S as m,i as l}from"./assets/vendor-5ObWk2rO.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&o(a)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();function d(s){return s.map(({webformatURL:r,largeImageURL:i,tags:o,likes:e,views:t,comments:a,downloads:h})=>`<li class="gallery--list">
+            <a class="gallery--link" href="${i}">
               <img
                 class="gallery--image"
-                src="${t}"
-                alt="${a}"
+                src="${r}"
+                alt="${o}"
                 width="360"
                 height="200"
               />
@@ -15,16 +15,16 @@ import{S as m,a as d,i as n}from"./assets/vendor-CUAzmrUA.js";(function(){const 
               </div>
               <div class="thumb--item">
                 <h2 class="thumb--tittle">Views</h2>
-                <p class="thumb--amount">${i}</p>
+                <p class="thumb--amount">${t}</p>
               </div>
               <div class="thumb--item">
                 <h2 class="thumb--tittle">Comments</h2>
-                <p class="thumb--amount">${s}</p>
+                <p class="thumb--amount">${a}</p>
               </div>
               <div class="thumb--item">
                 <h2 class="thumb--tittle">Downloads</h2>
                 <p class="thumb--amount">${h}</p>
               </div>
             </div>
-          </li>`).join("")}const u=document.querySelector(".gallery"),c=document.querySelector(".loader"),g=new m(".gallery a",{captions:!0,captionsData:"alt",captionDelay:250});function f(o){d.get(o).then(t=>{console.log(t),t.data.hits.length||n.warning({title:"Warning",message:"Sorry, there are no images matching your search query. Please try again!",titleColor:"white",messageColor:"white",titleSize:"12px",backgroundColor:"#ef4040",iconColor:"white",position:"topRight"}),c.style.display="block",u.innerHTML=p(t.data.hits),c.style.display="none",g.refresh()}).catch(t=>{n.error({title:"Error",message:"Something went wrong. Please try again later.",titleColor:"white",messageColor:"white",titleSize:"12px",backgroundColor:"#ef4040",iconColor:"white",position:"topRight"})})}alert("Коментар : Я не можу зрозуміти яку саме частину винести в файл api ,а яку в мейн , тикніть пальцем в потрібну частину коду , яку винести Переробив на axios і працює завантажувач , дальше ступор");const l=document.querySelector(".form"),y="48554296-d2b735e3ffae040ef4553c6bb";c.style.display="none";l.addEventListener("submit",o=>{o.preventDefault();let t=l.seurch.value.trim(),r=`https://pixabay.com/api/?key=${y}&q=${t}&image_type=photo&orientation=horizontal&safesearch=true`;if(u.innerHTML="",t.length===0||t.value===""){n.warning({title:"Warning",message:"Please, enter a value!",titleColor:"white",messageColor:"white",titleSize:"12px",backgroundColor:"#ef4040",iconColor:"white",position:"topRight"});return}f(r),l.reset()});
+          </li>`).join("")}async function p(s){const i=`https://pixabay.com/api/?key=48554296-d2b735e3ffae040ef4553c6bb&q=${s}&image_type=photo&orientation=horizontal&safesearch=true`,o=await fetch(i);if(!o.ok)throw new Error(o.statusText);return o.json()}const u=document.querySelector(".gallery"),c=document.querySelector(".loader"),n=document.querySelector(".form"),f=new m(".gallery a",{captions:!0,captionsData:"alt",captionDelay:250});n.addEventListener("submit",async s=>{s.preventDefault();const r=n.search.value.trim();if(c.classList.remove("hidden"),u.innerHTML="",r.length===0||r.value===""){l.warning({title:"Warning",message:"Please, enter a value!",titleColor:"white",messageColor:"white",titleSize:"12px",backgroundColor:"#ef4040",iconColor:"white",position:"topRight"}),c.classList.add("hidden");return}try{const i=await p(r);i.hits.length||l.warning({title:"Warning",message:"Sorry, there are no images matching your search query. Please try again!",titleColor:"white",messageColor:"white",titleSize:"12px",backgroundColor:"#ef4040",iconColor:"white",position:"topRight"}),u.innerHTML=d(i.hits),c.classList.add("hidden"),f.refresh(),n.reset()}catch{l.error({title:"Error",message:"Something went wrong. Please try again later.",titleColor:"white",messageColor:"white",titleSize:"12px",backgroundColor:"#ef4040",iconColor:"white",position:"topRight"}),n.reset()}});
 //# sourceMappingURL=index.js.map
