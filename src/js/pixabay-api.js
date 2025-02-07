232e6531a52eeea294969a6afadf762a1ddc4dd6
Viktor <victorharasymiv97@gmail.com>
2025-02-08 00:34:52 +0100

    done

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,73 +1,13 @@
-// Описаний у документації
-import iziToast from "izitoast";
-// Додатковий імпорт стилів
-import "izitoast/dist/css/iziToast.min.css";
+/*  Function  API  */
 
+export async function getImages(inputValue) {
+    
+    const apiKey = "48554296-d2b735e3ffae040ef4553c6bb";
+    const url = `https://pixabay.com/api/?key=${apiKey}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true`;
 
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-
-
-import { createMarkup } from "./render-functions";
-
-import axios from 'axios';
-
-/*   Variables   */ 
-
-export const listImages = document.querySelector('.gallery');
-export const loader = document.querySelector('.loader');
-
-const refreshPage = new SimpleLightbox('.gallery a', {
-    captions: true,
-    captionsData: 'alt',
-    captionDelay: 250,
-});
-
-/*  Function  */
-
-export function getImages(api) {
-    axios
-    .get(api)
-    .then((response) => { console.log(response)
-        if (!response.data.hits.length) {
-            iziToast.warning({
-               title: 'Warning',
-               message: 'Sorry, there are no images matching your search query. Please try again!',
-
-               titleColor: 'white',
-               messageColor: 'white',
-               titleSize: '12px',
-               backgroundColor: '#ef4040',
-
-               iconColor: 'white',
-               position: 'topRight'
-            });
-        }
-
-        loader.style.display = 'block';
-
-        listImages.innerHTML = ("beforeend", createMarkup(response.data.hits));
-
-        loader.style.display = 'none';
-
-        refreshPage.refresh();   
-    })
-    .catch((error) => {
-        iziToast.error({
-            title: 'Error',
-            message: 'Something went wrong. Please try again later.',
-
-            titleColor: 'white',
-            messageColor: 'white',
-            titleSize: '12px',
-            backgroundColor: '#ef4040',
-
-            iconColor: 'white',
-            position: 'topRight'
-        });
-    });
-}
-
-/* COMMENT HM */
-
-alert("Коментар : Я не можу зрозуміти яку саме частину винести в файл api ,а яку в мейн , тикніть пальцем в потрібну частину коду , яку винести Переробив на axios і працює завантажувач , дальше ступор");
+    const res = await fetch(url);
+        if (!res.ok) {
+            throw new Error(res.statusText);
+          }
+          return res.json();
+    }
--- src/main.js
+++ src/main.js
@@ -1,57 +1,100 @@
+/* Import */
 
-// Описаний у документації
 import iziToast from "izitoast";
-// Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
 
-import { listImages } from "./js/pixabay-api";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import { createMarkup } from "./js/render-functions";
 import { getImages } from "./js/pixabay-api";
-import { loader } from "./js/pixabay-api";
-
-
-/*   A P I  S E R V I E S   */ 
 
 /*   Variables   */ 
 
+const listImages = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+
 const form = document.querySelector(".form");
 
-const apiKey = "48554296-d2b735e3ffae040ef4553c6bb";
+/*  Setup for gallery render */
 
-/*  Styles  */
+const refreshPage = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+});
 
-loader.style.display = 'none';
+/*   A P I  S E R V I E S   */ 
 
 
 /*  Event Listener  */
 
-form.addEventListener("submit" , (event) => {
-     
+form.addEventListener("submit" , async (event) => {
     event.preventDefault();
+    const inputValue = form.search.value.trim();
 
-    let inputValue = form.seurch.value.trim();
-    let url = `https://pixabay.com/api/?key=${apiKey}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true`;
+    loader.classList.remove('hidden');
 
     listImages.innerHTML = "";
 
-
     if(inputValue.length === 0 || inputValue.value === "") {
 
-        iziToast.warning({
-            title: 'Warning',
-            message: 'Please, enter a value!',
+         iziToast.warning({
+             title: 'Warning',
+             message: 'Please, enter a value!',
 
-            titleColor: 'white',
+             titleColor: 'white',
             messageColor: 'white',
-            titleSize: '12px',
-            backgroundColor: '#ef4040',
+             titleSize: '12px',
+             backgroundColor: '#ef4040',
 
-            iconColor: 'white',
-            position: 'topRight'
-        });
+             iconColor: 'white',
+             position: 'topRight'
+         });
 
-        return;
+         loader.classList.add('hidden');
+
+         return;
     }
 
-    getImages(url)
-    form.reset()
+    try {
+        const data = await getImages(inputValue);
+
+        if (!data.hits.length) {
+            iziToast.warning({
+                   title: 'Warning',
+                   message: 'Sorry, there are no images matching your search query. Please try again!',
+    
+                   titleColor: 'white',
+                   messageColor: 'white',
+                   titleSize: '12px',
+                   backgroundColor: '#ef4040',
+    
+                   iconColor: 'white',
+                   position: 'topRight'
+            });
+        }
+    
+        listImages.innerHTML = ("beforeend", createMarkup(data.hits));
+
+        loader.classList.add('hidden');
+    
+        refreshPage.refresh();
+        form.reset();
+
+    } catch(error) {
+            iziToast.error({
+                title: 'Error',
+                message: 'Something went wrong. Please try again later.',
+    
+                titleColor: 'white',
+                messageColor: 'white',
+                titleSize: '12px',
+                backgroundColor: '#ef4040',
+    
+                iconColor: 'white',
+                position: 'topRight'
+            });
+            form.reset()
+        }
 });
